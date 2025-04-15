4d1e1a7208ca42edb0093da33c48e9442b30720a
Elias R <elias.ravarotto@gmail.com>
2025-04-14 21:40:58 -0300

    fix: ajuste en vistas

--- resources/views/admin/includes/navbar.blade.php
+++ resources/views/admin/includes/navbar.blade.php
@@ -17,7 +17,7 @@
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="{{ route('my_panel') }}">Mi panel</a></li>
+                <li><a class="dropdown-item" href="{{ route('dashboard') }}"><i class="fas fa-tachometer-alt"></i> Panel</a></li>
                 <li><hr class="dropdown-divider" /></li>
                 <li><a class="dropdown-item" href="{{ route('show_account', Auth::user()->id) }}">Mi cuenta</a></li>
                 <li><a class="dropdown-item" href="{{ route('profile_form_change_password', Auth::user()->id) }}">Cambiar Contraseña</a></li>
